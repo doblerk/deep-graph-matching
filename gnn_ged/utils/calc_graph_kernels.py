2e0fa889d97c7dc75928f707d50dc93bff13c1fe
Kalvin Dobler <kalvindobler@hotmail.com>
2024-11-13 13:53:08 +0100

    improved framework

--- gnn_ged/utils/calc_graph_kernels.py
+++ gnn_ged/utils/calc_graph_kernels.py
@@ -18,34 +18,36 @@ def get_args_parser():
 def main(args):
 
     dataset = fetch_dataset(name=args.dataset_name, 
-                            verbose=False,
+                            verbose=True,
                             data_home=args.dataset_dir,
+                            produce_labels_nodes=True,
                             as_graphs=False)
+    print(dataset.data[0])
+    # match args.kernel:
 
-    match args.kernel:
-
-        case 'sp':
-            kernel = GraphKernel(kernel={'name':'shortest_path', 'algorithm_type':'floyd_warshall'}, normalize=True)
+    #     case 'sp':
+    #         kernel = GraphKernel(kernel={'name':'shortest_path', 'algorithm_type':'floyd_warshall'}, normalize=True)
         
-        case 'rw':
-            kernel = GraphKernel(kernel={'name':'random_walk'}, normalize=True)
+    #     case 'rw':
+    #         kernel = GraphKernel(kernel={'name':'random_walk'}, normalize=True)
         
-        case 'wl':
-            kernel = GraphKernel(kernel={'name':'weisfeiler_lehman', 'n_iter':5}, normalize=True)
+    #     case 'wl':
+    #         kernel = GraphKernel(kernel={'name':'weisfeiler_lehman', 'n_iter':5}, normalize=True)
 
-    t0 = time()
-    similarity_matrix = kernel.fit_transform(dataset.data)
-    dissimilarity_matrix = np.max(similarity_matrix) - similarity_matrix
-    t1 = time()
-    computation_time = t1 - t0
+    # # t0 = time()
+    # similarity_matrix = kernel.fit_transform(dataset.data)
+    # dissimilarity_matrix = np.max(similarity_matrix) - similarity_matrix
+    # # t1 = time()
+    # # computation_time = t1 - t0
+    # print(similarity_matrix)
 
-    dissimilarity_matrix[dissimilarity_matrix < 0.0] = 0.0
-    dissimilarity_matrix[dissimilarity_matrix == 'NaN'] = 1.0
+    # dissimilarity_matrix[dissimilarity_matrix < 0.0] = 0.0
+    # dissimilarity_matrix[dissimilarity_matrix == 'NaN'] = 1.0
 
-    with open(os.path.join(args.output_dir, f'computation_time_kernels.txt'), 'a') as file:
-        file.write(f'Computation time for {args.kernel} kernel: {computation_time}\n')
+    # with open(os.path.join(args.output_dir, f'computation_time_kernels.txt'), 'a') as file:
+    #     file.write(f'Computation time for {args.kernel} kernel: {computation_time}\n')
 
-    np.save(os.path.join(args.output_dir, f'{args.kernel}_distances.npy'), dissimilarity_matrix)
+    # np.save(os.path.join(args.output_dir, f'{args.kernel}_distances.npy'), dissimilarity_matrix)
 
 
 if __name__ == '__main__':
